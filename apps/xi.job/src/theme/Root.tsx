e4bd5e7b6bc4180213d924e92f2b77049b1a2b55
dushnyy <archi3run@gmail.com>
2023-06-26 17:56:27 +0300

    feat: provide theme

---
+++ apps/xi.job/src/theme/Root.tsx
@@ -0,0 +1,14 @@
+import React, { PropsWithChildren } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { getScheme } from "./theme";
+
+const theme = () => createTheme(getScheme("light"));
+const Root: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </>
+  );
+};
+
+export default Root;
