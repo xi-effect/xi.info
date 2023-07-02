11e5ac6f64d638f9a86bd24098310855c1d29873
unknownproperty <igor.buksheff@yandex.ru>
2023-07-02 23:05:15 +0300

    feat: add xi.vacancy

---
+++ apps/xi.vacancy/app/layout.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import './globals.css';
+
+import { Inter } from 'next/font/google';
+
+import { ReactNode } from 'react';
+import { ThemeRegistry } from 'pkg.theme';
+
+const inter = Inter({
+  weight: ['400', '500', '600', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en" className={`${inter.variable}`}>
+      <body>
+        <ThemeRegistry mode="light">
+          <main className="w-screen h-screen">{children}</main>
+        </ThemeRegistry>
+      </body>
+    </html>
+  );
+}
