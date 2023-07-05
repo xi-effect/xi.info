750340d5519e156dad9983d8bb919861bafd217e
dushnyy <archi3run@gmail.com>
2023-07-05 18:26:28 +0300

    fix: build error

--- apps/xi.vacancy/components/Footer.tsx
+++ apps/xi.vacancy/components/Footer.tsx
@@ -1,32 +1,37 @@
 import Image from 'next/image';
-const Footer = () => {
-  return (
-    <footer>
-      <div className="my-16 p-16 bg-gray-10 rounded-[32px] relative">
-        <h4 className="text-[80px] font-semibold mb-16">Приходи работать вместе</h4>
 
-        <p className="mb-16 text-5xl max-w-4xl">
-          Напиши пару слов о себе и о том, какая вакансия может быть вам интересна
-        </p>
+const Footer = () => (
+  <footer>
+    <div className="my-16 p-16 bg-gray-10 rounded-[32px] relative">
+      <h4 className="text-[80px] font-semibold mb-16">Приходи работать вместе</h4>
 
-        <button className="bg-brand-80 text-gray-0 px-16 py-6 rounded-3xl t-l h-[112px] w-[507px]">
-          Отправить резюме
-        </button>
+      <p className="mb-16 text-5xl max-w-4xl">
+        Напиши пару слов о себе и о том, какая вакансия может быть вам интересна
+      </p>
 
-        <img
-          alt="team working"
-          src="/work-together.jpg"
-          className="absolute top-0 right-0 object-cover w-[600px] h-full rounded-r-3xl"
-        />
-      </div>
+      <button
+        type="button"
+        className="bg-brand-80 text-gray-0 px-16 py-6 rounded-3xl t-l h-[112px] w-[507px]"
+      >
+        Отправить резюме
+      </button>
 
-      <div className="p-16 flex items-center justify-between">
-        <Image src="/team.svg" width={219} height={32} alt="logo xi.effect" />
+      <img
+        alt="team working"
+        src="/work-together.jpg"
+        className="absolute top-0 right-0 object-cover w-[600px] h-full rounded-r-3xl"
+      />
+    </div>
 
-        <span className="t-xl text-gray-60"> &copy; xieffect.ru {new Date().getFullYear()}</span>
-      </div>
-    </footer>
-  );
-};
+    <div className="p-16 flex items-center justify-between">
+      <Image src="/team.svg" width={219} height={32} alt="logo xi.effect" />
+
+      <span className="t-xl text-gray-60">
+        &copy; xieffect.ru
+        {new Date().getFullYear()}
+      </span>
+    </div>
+  </footer>
+);
 
 export default Footer;
--- apps/xi.vacancy/components/Header.tsx
+++ apps/xi.vacancy/components/Header.tsx
@@ -1,19 +1,19 @@
 import Image from 'next/image';
 
-const Header = () => {
-  return (
-    <header className="flex justify-between items-center p-16">
-      <Image src="/team.svg" width={219} height={32} alt="logo xi.effect" />
+const Header = () => (
+  <header className="flex justify-between items-center p-16">
+    <Image src="/team.svg" width={219} height={32} alt="logo xi.effect" />
 
-      <nav className="flex items-center">
-        <a href="#" className="mr-16 t-xl text-gray-60">
-          Вакансии
-        </a>
+    <nav className="flex items-center">
+      <a href="#" className="mr-16 t-xl text-gray-60">
+        Вакансии
+      </a>
 
-        <button className="btn-l bg-brand-80 text-gray-0">Отправить резюме</button>
-      </nav>
-    </header>
-  );
-};
+      <button type="button" className="btn-l bg-brand-80 text-gray-0">
+        Отправить резюме
+      </button>
+    </nav>
+  </header>
+);
 
 export default Header;
