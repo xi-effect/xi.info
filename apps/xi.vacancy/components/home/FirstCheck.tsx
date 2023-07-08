d630a724c3da627eaa2ab40dd4fbe50a162098fa
dushnyy <archi3run@gmail.com>
2023-07-08 20:20:02 +0300

    feat: section about check yourself

---
+++ apps/xi.vacancy/components/home/FirstCheck.tsx
@@ -0,0 +1,53 @@
+const FirstCheck = () => (
+  <section className="py-8 px-4 xl:p-16">
+    <h2 className="font-semibold mb-[48px] text-[32px] sm:text-[64px] xl:text-[80px]">
+      Для начала проверь, подходим ли мы тебе
+    </h2>
+
+    <div className="flex flex-col xl:flex-row xl:justify-between mb-[48px] sm:mb-16">
+      <h3 className="font-semibold mb-[16px] xl:mb-0 text-[24px] sm:text-[48px] xl:basis-2/4">
+        Доверяем сотрудникам
+      </h3>
+
+      <p className="text-[16px] sm:text-[40px] xl:basis-2/4">
+        Мы отвечаем за результат, видим новые пути развития, аргументированно предлагаем из команде
+        и реализуем. Чем выше результат и ответственность, тем больше свободы в принятии решения.
+      </p>
+    </div>
+
+    <div className="flex flex-col xl:flex-row xl:justify-between mb-[48px] sm:mb-16">
+      <h3 className="font-semibold mb-[16px] xl:mb-0 text-[24px] sm:text-[48px] xl:basis-2/4">
+        Создаём новое
+      </h3>
+
+      <p className="text-[16px] sm:text-[40px] xl:basis-2/4">
+        Опираемся на собственный опыт и опыт профессионального сообщества. Учимся на ошибках и
+        воспринимаем критику как топливо для развития и совершенствования.
+      </p>
+    </div>
+
+    <div className="flex flex-col xl:flex-row xl:justify-between mb-[48px] sm:mb-16">
+      <h3 className="font-semibold mb-[16px] xl:mb-0 text-[24px] sm:text-[48px] xl:basis-2/4">
+        Видим важное в мелочах
+      </h3>
+
+      <p className="text-[16px] sm:text-[40px] xl:basis-2/4">
+        Мы уделяем внимание мелочам, стремимся довести работу до идеала. Эта общая ценность
+        объединяет нас, помогает давать и принимать обратную связь и находить общие интересы.
+      </p>
+    </div>
+
+    <div className="flex flex-col xl:flex-row xl:justify-between">
+      <h3 className="font-semibold mb-[16px] xl:mb-0 text-[24px] sm:text-[48px] xl:basis-2/4">
+        Учимся и растём
+      </h3>
+
+      <p className="text-[16px] sm:text-[40px] xl:basis-2/4">
+        Мы развиваемся и обмениваемся опытом. Изучаем новое, чтобы быть готовыми к прогрессу,
+        переменам и решениям будущего.
+      </p>
+    </div>
+  </section>
+);
+
+export default FirstCheck;
