3a6d85771b3ae38d91833414073fc840e0f217bb
dushnyy <archi3run@gmail.com>
2023-07-08 20:18:08 +0300

    fix: refactoring

--- apps/xi.vacancy/app/layout.tsx
+++ apps/xi.vacancy/app/layout.tsx
@@ -5,14 +5,20 @@ import { ThemeRegistry } from 'pkg.theme';
 import { inter, marker } from 'fonts/fonts';
 
 import '@xipkg/tailwind/tailwind.css';
+import Header from '../components/layout/Header';
+import Footer from '../components/layout/Footer';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${inter.variable} ${marker.variable}`}>
-      <body>
+      <body className="overflow-x-hidden">
         <ThemeRegistry mode="light">
-          <main className="w-screen h-screen container max-w-[1920px] mx-auto px-4 sm:px-8 md:px-16 flex flex-col">
+          <main className="w-screen h-screen container max-w-[1920px] mx-auto px-4 sm:px-8 md:px-16">
+            <Header />
+
             {children}
+
+            <Footer />
           </main>
         </ThemeRegistry>
       </body>
--- apps/xi.vacancy/app/page.tsx
+++ apps/xi.vacancy/app/page.tsx
@@ -1,16 +1,7 @@
 'use client';
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import HomeWrapper from '../components/home';
 
-export default function Home() {
-  return (
-    <>
-      <Header />
+const Home = () => <HomeWrapper />;
 
-      <section className="flex-auto ">тук тук</section>
-
-      <Footer />
-    </>
-  );
-}
+export default Home;
