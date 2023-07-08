ce40aa3552e3ad7ff2b5618f0799f738e35afa9e
dushnyy <archi3run@gmail.com>
2023-07-08 20:20:30 +0300

    feat: home page wrapper

---
+++ apps/xi.vacancy/components/home/index.tsx
@@ -0,0 +1,21 @@
+import FindYourCase from './FindYourCase';
+import EducationalPlatform from './EducationalPlatform';
+import FirstCheck from './FirstCheck';
+import AppreciateTheTeam from './AppreciateTheTeam';
+import WorkForSpecialists from './WorkForSpecialists';
+
+const Home = () => (
+  <>
+    <FindYourCase />
+
+    <EducationalPlatform />
+
+    <FirstCheck />
+
+    <AppreciateTheTeam />
+
+    <WorkForSpecialists />
+  </>
+);
+
+export default Home;
