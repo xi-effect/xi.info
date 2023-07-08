bdd92cf586e857dc0ab21a81a89d5dbb286b3bfe
dushnyy <archi3run@gmail.com>
2023-07-08 11:18:05 +0300

    fix: font settings

--- apps/xi.vacancy/app/layout.tsx
+++ apps/xi.vacancy/app/layout.tsx
@@ -1,23 +1,14 @@
 'use client';
 
-import '@xipkg/tailwind/tailwind.css';
-
-import { Inter } from 'next/font/google';
-
 import { ReactNode } from 'react';
 import { ThemeRegistry } from 'pkg.theme';
+import { inter, marker } from 'fonts/fonts';
 
-const inter = Inter({
-  weight: ['400', '500', '600', '700'],
-  style: ['normal'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter',
-});
+import '@xipkg/tailwind/tailwind.css';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={`${inter.variable}`}>
+    <html lang="en" className={`${inter.variable} ${marker.variable}`}>
       <body>
         <ThemeRegistry mode="light">
           <main className="w-screen h-screen container max-w-[1920px] mx-auto px-4 sm:px-8 md:px-16 flex flex-col">
--- apps/xi.vacancy/tailwind.config.js
+++ apps/xi.vacancy/tailwind.config.js
@@ -1,6 +1,18 @@
 /* eslint-disable global-require */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', '../../packages/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    '../../packages/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['var(--font-inter)'],
+        marker: ['var(--font-marker)'],
+      },
+    },
+  },
   presets: [require('@xipkg/tailwind/design-system-preset')],
 };
