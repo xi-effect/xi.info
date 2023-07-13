6586786dcba91af0c11dd774ff053510b8cc030d
KashVer <veronika.kashkadova55@gmail.com>
2023-07-13 15:08:20 +0300

    feat(11377910): edit next.config & add preset

--- apps/xi.land/next.config.js
+++ apps/xi.land/next.config.js
@@ -1,4 +1,77 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// Не поддавайтесь соблазну использовать здесь import
+const path = require('path');
+const runtimeCaching = require('next-pwa/cache');
 
-module.exports = nextConfig
+const plugins = [];
+
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    register: true,
+    runtimeCaching,
+    mode: 'production',
+    reloadOnOnline: true,
+    cacheOnFrontEndNav: true,
+    disable: process.env.NODE_ENV === 'development',
+    skipWaiting: true,
+    sw: '/sw.js',
+    buildExcludes: [
+        /middleware-manifest\.json$/,
+        /_middleware\.js$/,
+        /_middleware\.js\.map$/,
+        /middleware-runtime\.js$/,
+        /server\/pages\/_middleware\.js$/,
+    ],
+});
+
+plugins.push(withPWA);
+
+const nextConfig = {
+    experimental: {
+        esmExternals: true,
+        outputFileTracingRoot: path.join(__dirname, '../../'),
+    },
+    transpilePackages: [
+        'pkg.theme',
+        'pkg.spinner',
+        'pkg.utils',
+        '@xipkg/config.typescript',
+        'pkg.signin.form',
+        '@emotion/styled',
+        '@emotion/react',
+        '@emotion/cache',
+        '@mui/material',
+    ],
+    compiler: {
+        removeConsole: process.env.NODE_ENV !== 'development',
+    },
+    reactStrictMode: true,
+    images: {
+        domains: [
+            'cdn.discordapp.com',
+            'localhost:3000',
+            'localhost:5000',
+            'xieffect.ru:5000',
+            'xieffect.ru',
+        ],
+    },
+    output: 'standalone',
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            issuer: { and: [/\.(js|ts|md)x?$/] },
+            type: 'asset/resource',
+        });
+        config.module.rules.push({
+            test: /\.d.ts?$/,
+            use: [
+                {
+                    loader: 'ignore-loader',
+                },
+            ],
+        });
+
+        return config;
+    },
+};
+
+module.exports = () => plugins.reduce((acc, next) => next(acc), nextConfig);
