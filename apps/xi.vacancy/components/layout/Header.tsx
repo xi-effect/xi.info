7824426439b5eb31a4c3d9c96e973b5362248fda
dushnyy <archi3run@gmail.com>
2023-07-23 14:33:29 +0300

    fix: refactoring

--- apps/xi.vacancy/components/layout/Header.tsx
+++ apps/xi.vacancy/components/layout/Header.tsx
@@ -1,26 +1,40 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-const Header = () => (
-  <header className="flex justify-between items-center p-8 sm:p-16">
-    <Image
-      width={219}
-      height={32}
-      quality={100}
-      alt="logo xi.effect"
-      src="/icons/team.svg"
-      className="w-[165px] h-[24px] sm:w-[219px] sm:h-[32px]"
-    />
+const Header = () => {
+  const pathname = usePathname();
 
-    <nav className="flex items-center hidden md:block">
-      <a href="apps/xi.vacancy/components#" className="mr-16 t-xl text-gray-60">
-        Вакансии
-      </a>
+  return (
+    <header className="flex justify-between items-center py-8 px-4 sm:py-16 xl:px-16">
+      <Link href="/">
+        <Image
+          width={219}
+          height={32}
+          quality={100}
+          alt="logo xi.effect"
+          src="/icons/team.svg"
+          className="w-[165px] h-[24px] sm:w-[219px] sm:h-[32px]"
+        />
+      </Link>
 
-      <button type="button" className="btn-l bg-brand-80 text-gray-0 text-[22px]">
-        Отправить резюме
-      </button>
-    </nav>
-  </header>
-);
+      <nav className="flex items-center hidden md:block">
+        <Link
+          href="/vacancy"
+          className={`
+          ${pathname === '/vacancy' ? 'text-brand-80' : 'text-gray-60'}
+           transition-colors mr-16 t-xl
+           `}
+        >
+          Вакансии
+        </Link>
+
+        <button type="button" className="btn-l bg-brand-80 text-gray-0 text-[22px]">
+          Отправить резюме
+        </button>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
