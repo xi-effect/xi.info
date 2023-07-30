3b15813dc7c5363c8b593f4effc1007d029a0e0c
dushnyy <archi3run@gmail.com>
2023-07-30 17:59:23 +0300

    fix: settings for default tags

--- apps/xi.vacancy/mdx-components.tsx
+++ apps/xi.vacancy/mdx-components.tsx
@@ -1,7 +1,17 @@
 import type { MDXComponents } from 'mdx/types';
+import { FC, PropsWithChildren } from 'react';
+
+const H2: FC<PropsWithChildren> = ({ children }) => <h2 className="mdx-h2">{children}</h2>;
+
+const P: FC<PropsWithChildren> = ({ children }) => <p className="mdx-p">{children}</p>;
+
+const Li: FC<PropsWithChildren> = ({ children }) => <li className="mdx-li">{children}</li>;
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h2: H2,
+    p: P,
+    li: Li,
     ...components,
   };
 }
