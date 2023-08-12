d91127ff02a2daaa46e5d0c721d533ba388959fa
dushnyy <archi3run@gmail.com>
2023-08-12 17:36:42 +0300

    feat: component for resume form

---
+++ apps/xi.vacancy/components/SendResumeButton/Select.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { Control, Controller } from 'react-hook-form';
+import { FormDataT } from './index';
+
+type SelectT = {
+  error?: boolean;
+  helperText?: string;
+  control: Control<FormDataT, any>;
+};
+
+const selectOptions = [
+  {
+    title: 'Разработка',
+    value: 'development',
+  },
+  {
+    title: 'Дизайн',
+    value: 'design',
+  },
+  {
+    title: 'Продукт',
+    value: 'product',
+  },
+  {
+    title: 'Маркетинг',
+    value: 'marketing',
+  },
+];
+
+const Select: React.FC<SelectT> = (props) => {
+  const { control, error, helperText } = props;
+
+  const [open, setOpen] = useState<boolean>(false);
+
+  const [currentVacancy, setCurrentVacancy] = useState<string | null>(null);
+
+  const listener = (e) => {
+    const target = e.target as HTMLElement;
+
+    if (!target.closest('.resume-select')) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', listener);
+
+    return () => window.removeEventListener('click', listener);
+  }, []);
+
+  return (
+    <Controller
+      name="vacancy"
+      control={control}
+      render={({ field: { onChange } }) => (
+        <div className="relative flex flex-col text-[16px] mb-[16px] sm:mb-[24px] relative">
+          <span className="leading-[22px]">Интересующая специализация</span>
+          <button
+            type="button"
+            aria-expanded={!open}
+            onClick={() => setOpen((v) => !v)}
+            className={`
+            ${currentVacancy ? 'text-gray-100' : `${helperText ? 'text-red-60' : 'text-gray-30'}`}  
+            ${error ? 'border-red-20' : 'border-gray-30 '}
+            
+            sm:p-[12px] resume-select cursor-pointer flex bg-gray-0 justify-between content-center mt-2 inline-block text-[16px] w-full px-[10px] h-[32px] sm:h-[48px] rounded-[8px] border-2`}
+          >
+            {helperText || currentVacancy || 'Выберите'}
+
+            <Image
+              width={20}
+              height={20}
+              src="/icons/drop.svg"
+              alt={open ? 'list is open' : 'list is closed'}
+              className={`${open ? 'rotate-180' : 'rotate-0'} h-full transition-transform`}
+            />
+          </button>
+          <div
+            aria-hidden={!open}
+            className={` ${
+              open ? 'visible opacity-1 top-[83px]' : 'invisible opacity-0 top-[100px]'
+            } z-[101] resume-select transition-all absolute w-full  bg-gray-0 p-[12px] rounded-[8px] border-2 border-gray-30`}
+          >
+            {selectOptions.map(({ value, title }) => (
+              <button
+                type="button"
+                onClick={() => {
+                  setOpen(false);
+                  setCurrentVacancy(title);
+                  onChange(value);
+                }}
+                className="rounded-[8px]  p-[12px] transition-colors focus:text-gray-0 focus:bg-brand-80  hover:text-gray-0 hover:bg-brand-80 w-full bg-gray-0 text-left"
+              >
+                {title}
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
+    />
+  );
+};
+
+export default Select;
---
+++ apps/xi.vacancy/components/SendResumeButton/UploadFile.tsx
@@ -0,0 +1,82 @@
+import { Control, Controller } from 'react-hook-form';
+import React, { useRef, useState, DragEvent } from 'react';
+import Image from 'next/image';
+import { FormDataT } from './index';
+
+type UploadFileT = {
+  control: Control<FormDataT, any>;
+};
+
+const UploadFile: React.FC<UploadFileT> = (props) => {
+  const { control } = props;
+
+  const [drag, setDrag] = useState<boolean>(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onDragStart = (e: DragEvent<HTMLLabelElement>) => {
+    e.preventDefault();
+    setDrag(true);
+  };
+
+  const onDragLeave = (e: DragEvent<HTMLLabelElement>) => {
+    e.preventDefault();
+    setDrag(false);
+  };
+
+  const onDragDesc = drag ? (
+    <>
+      Отпустите файл
+      <span className="text-gray-60 ml-[4px]">чтобы загрузить</span>
+    </>
+  ) : (
+    <>
+      Выберите файл
+      <span className="text-gray-60 ml-[4px]">или перетащите сюда</span>
+    </>
+  );
+
+  return (
+    <Controller
+      name="file"
+      control={control}
+      render={({ field: { onChange, value } }) => (
+        <div className=" flex flex-col text-[16px] mb-[16px] sm:mb-[24px]">
+          <span className="leading-[22px]">Приложи файлы</span>
+
+          <label
+            onDrop={(e) => {
+              e.preventDefault();
+              onChange(e.dataTransfer.files[0]);
+            }}
+            htmlFor="file"
+            onDragStart={onDragStart}
+            onDragLeave={onDragLeave}
+            onDragOver={onDragStart}
+            className="cursor-pointer flex h-[32px] sm:h-[48px] mt-2 w-full border-[1px] rounded-[6px] border-gray-40 border-dashed px-[8px] py-[5px] text-[14px] text-gray-100 sm:p-[12px]"
+          >
+            {value ? value.name : onDragDesc}
+
+            <Image
+              width={16}
+              height={16}
+              src="/icons/upload.svg"
+              alt="upload file"
+              className="ml-auto"
+            />
+
+            <input
+              type="file"
+              id="file"
+              ref={inputRef}
+              className="hidden"
+              onChange={(e) => onChange(e.currentTarget.files?.[0])}
+            />
+          </label>
+        </div>
+      )}
+    />
+  );
+};
+
+export default UploadFile;
