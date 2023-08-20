fef65f4d2437bc4f5d1a4e529cc60a3b5e55d6cc
dushnyy <archi3run@gmail.com>
2023-08-20 14:17:18 +0300

    fix: refactoring

--- apps/xi.vacancy/components/layout/Header.tsx
+++ apps/xi.vacancy/components/layout/Header.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import SendResumeButton from '../SendResumeButton';
+import SendResumeButton from '../modal/SendResumeButton';
 
 const Header = () => {
   const pathname = usePathname();
