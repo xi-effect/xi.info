457e02da655240eb94089ba8554140d7fa1c537e
dushnyy <archi3run@gmail.com>
2023-08-20 14:16:33 +0300

    fix: added text with spaces from figma

--- apps/xi.vacancy/components/home/FirstCheck.tsx
+++ apps/xi.vacancy/components/home/FirstCheck.tsx
@@ -1,8 +1,8 @@
 const FirstCheck = () => (
   <section className="p-[16px] sm:p-8 mb-[38px] sm:mb-[56px] 2xl:py-[64px] 2xl:px-[54px]">
-    <h2 className="font-semibold leading-[110%] mb-[16px] sm:mb-8 2xl:mb-[48px] text-[24px] sm:text-[48px] 2xl:text-[64px] min-[1700px]:flex">
-      <span className="block mr-[4px]">Для начала проверь, </span>
-      подходим ли мы тебе
+    <h2 className="tracking-wide font-semibold leading-[110%] mb-[16px] sm:mb-8 2xl:mb-[48px] text-[24px] sm:text-[48px] 2xl:text-[64px] min-[1700px]:flex">
+      <span className="block mr-[4px]">Для начала проверь, </span>
+      подходим ли мы тебе
     </h2>
 
     <div className="flex flex-col xl:flex-row mb-[24px] sm:mb-[48px]">
@@ -11,8 +11,8 @@ const FirstCheck = () => (
       </h3>
 
       <p className="leading-[130%] sm:leading-[130%] text-[16px] sm:text-[24px] 2xl:text-[28px] xl:basis-[47%] 2xl:basis-full sm:w-[90%]">
-        Мы отвечаем за результат, видим новые пути развития, аргументированно предлагаем из команде
-        и реализуем. Чем выше результат и ответственность, тем больше свободы в принятии решения.
+        Мы отвечаем за результат, видим новые пути развития, аргументированно предлагаем из команде
+        и реализуем. Чем выше результат и ответственность, тем больше свободы в принятии решения.
       </p>
     </div>
 
@@ -22,30 +22,30 @@ const FirstCheck = () => (
       </h3>
 
       <p className="leading-[130%] text-[16px] w-[90%] sm:text-[24px] 2xl:text-[28px] xl:basis-[47%] 2xl:basis-full">
-        Опираемся на собственный опыт и опыт профессионального сообщества. Учимся на ошибках и
-        воспринимаем критику как топливо для развития и совершенствования.
+        Опираемся на собственный опыт и опыт профессионального сообщества. Учимся на ошибках
+        и воспринимаем критику как топливо для развития и совершенствования.
       </p>
     </div>
 
     <div className="flex flex-col xl:flex-row  mb-[24px] sm:mb-[48px]">
       <h3 className="leading-[110%] font-semibold mb-[16px] xl:mb-0 text-[20px] sm:text-[32px] xl:basis-[47%] 2xl:basis-2/4">
-        Видим важное в мелочах
+        Видим важное в мелочах
       </h3>
 
       <p className="leading-[130%] text-[16px] sm:text-[24px] 2xl:text-[28px] xl:basis-[47%] 2xl:basis-full">
-        Мы уделяем внимание мелочам, стремимся довести работу до идеала. Эта общая ценность
-        объединяет нас, помогает давать и принимать обратную связь и находить общие интересы.
+        Мы уделяем внимание мелочам, стремимся довести работу до идеала. Эта общая ценность
+        объединяет нас, помогает давать и принимать обратную связь и находить общие интересы.
       </p>
     </div>
 
     <div className="flex flex-col xl:flex-row ">
       <h3 className="leading-[110%] font-semibold mb-[16px] xl:mb-0 text-[20px] sm:text-[32px] xl:basis-[47%] 2xl:basis-2/4">
-        Учимся и растём
+        Учимся и растём
       </h3>
 
       <p className="leading-[130%] sm:leading-[130%] text-[16px] sm:text-[24px] 2xl:text-[28px] xl:basis-[47%] 2xl:basis-full">
-        Мы развиваемся и обмениваемся опытом. Изучаем новое, чтобы быть готовыми к прогрессу,
-        переменам и решениям будущего.
+        Мы развиваемся и обмениваемся опытом. Изучаем новое, чтобы быть готовыми к прогрессу,
+        переменам и решениям будущего.
       </p>
     </div>
   </section>
