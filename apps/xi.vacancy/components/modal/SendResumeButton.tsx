1620536d4f4963639dd09a545012a8a45d5d18a8
unknownproperty <igor.buksheff@yandex.ru>
2023-09-05 19:07:23 +0300

    feat: change base url

--- apps/xi.vacancy/components/modal/SendResumeButton.tsx
+++ apps/xi.vacancy/components/modal/SendResumeButton.tsx
@@ -52,7 +52,7 @@ const SendResumeButton: FC<SendResumeButtonT> = (props) => {
   const onSubmit: SubmitHandler<FormDataT> = async (data) => {
     console.log(data);
     reset();
-    let response = await fetch('https://xieffect.ru:5000/webhooks/resume/', {
+    let response = await fetch('https://api.staging.xieffect.ru/webhooks/resume/', {
       method: 'POST',
       cache: 'no-cache',
       credentials: 'include',
