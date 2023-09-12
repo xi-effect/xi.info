388d8dd1067887ffe18cd623037b7f3e7e16d674
unknownproperty <igor.buksheff@yandex.ru>
2023-09-12 19:24:00 +0300

    fix: update text in alert

--- apps/xi.vacancy/components/modal/SendResumeButton.tsx
+++ apps/xi.vacancy/components/modal/SendResumeButton.tsx
@@ -67,7 +67,7 @@ const SendResumeButton: FC<SendResumeButtonT> = (props) => {
       // получаем тело ответа (см. про этот метод ниже)
       let json = await response.json();
       console.log('json', json);
-      alert('Спасибо, мы получили ваше резюме и свяжемся с вами')
+      alert('Спасибо, мы получили ваше резюме!')
     } else {
       alert('Ошибка HTTP: ' + response.status);
     }
