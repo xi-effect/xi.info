43bcdb1cd665547398f111c1722fe4692c56721d
Alexander Kovzan <alexanderkovzan@gmail.com>
2023-11-06 19:00:55 +0300

    feat(18927724): remove telegram chat link

--- apps/xi.vacancy/app/not-found.tsx
+++ apps/xi.vacancy/app/not-found.tsx
@@ -20,23 +20,17 @@ export default function NotFound() {
             <div className="py-32 flex flex-col items-start justify-center">
               <h1 className="flex flex-col-reverse gap-x-1 sm:inline-block text-[40px] leading-[48px] sm:text-[48px] sm:leading-[62px] lg:text-[64px] font-medium text-gray-100 mb-4">
                 Страница затерялась в облаках{' '}
-                <span className="inline-block text-l leading-[100%] align-super lg:text-h5 font-bold text-gray-30">404</span>
+                <span className="inline-block text-l leading-[100%] align-super lg:text-h5 font-bold text-gray-30">
+                  404
+                </span>
               </h1>
 
               <div className="text-[20px] leading-[26px] lg:text-[30px] lg:leading-9 text-gray-80">
                 В адресе есть ошибка или страница удалена
               </div>
               <div className="my-16">
-                <div>Если ошибка повторяется — напишите нам об этом</div>
                 <div>
-                  в{' '}
-                  <Link className="text-brand-80" href="https://t.me/+lENKCBU2OJxiNmIy">
-                    <Telegram className="inline-block fill-brand-80" />
-                    <span className='inline-block relative ml-1 after:block after:content-[""] after:left-0 after:bottom-0 after:w-full after:h-[1px] after:bg-brand-20'>
-                      чат Telegram
-                    </span>
-                  </Link>{' '}
-                  или{' '}
+                  Если ошибка повторяется — напишите нам об этом{' '}
                   <Link className="text-brand-80" href="xieffect@yandex.ru">
                     <Mail className="inline-block fill-brand-80" />
                     <span className='inline-block relative ml-1 after:block after:content-[""] after:left-0 after:bottom-0 after:w-full after:h-[1px] after:bg-brand-20'>
