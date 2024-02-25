2141cb7143fbb890057c349cb0a3baf8aa7959a7
Vitaly Kharlakov <vkharlakov@Vitalys-MacBook-Air.local>
2024-02-25 01:15:46 +0100

    feat(29044294): add adaptive layout up to 1920px

--- apps/xi.support/components/home-page/page.tsx
+++ apps/xi.support/components/home-page/page.tsx
@@ -6,16 +6,16 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <section className="flex items-center py-8 px-4 sm:bg-[url('/assets/home-page/home-page-bg.jpeg')] bg-no-repeat bg-[0%_37%] bg-[length:135%] sm:px-40 w-full sm:h-[1000px]">
-      <div>
-        <div className="text-[24px] leading-[31.2px] sm:text-[64px] sm:leading-[83.2px] text-gray-100 sm:text-gray-0 font-medium">
+    <section className="flex items-center py-8 px-4 sm:bg-[url('/assets/home-page/home-page-bg.jpeg')] bg-no-repeat sm:bg-[35%_65%] md:bg-[0%_37%] sm:bg-[length:220%] md:bg-[length:135%] sm:px-8 lg:px-40 w-full sm:h-[700px] lg:h-[1000px]">
+      <div className="w-full">
+        <div className="text-[24px] leading-[31.2px] sm:text-[48px] md:text-[64px] sm:leading-[62.4px] md:leading-[83.2px] text-gray-100 sm:text-gray-0 font-medium">
           <h2>Привет 👋</h2>
           <h2>Это база знаний xi.effect</h2>
         </div>
-        <p className="text-[14px] leading-[18.2px] mb-4 sm:text-2xl font-normal sm:mb-8 text-gray-90 sm:text-gray-10">
+        <p className="text-[14px] leading-[18.2px] mb-4 sm:text-[20px] sm:leading-[32px] md:text-2xl font-normal sm:mb-8 text-gray-90 sm:text-gray-10">
           Воспользуйтесь поиском или перейдите в нужный раздел
         </p>
-        <form className="h-12 w-full sm:w-[800px] sm:h-16 flex bg-gray-0 rounded-[13.71px] items-center px-3 sm:px-4 border-[2.67px] border-gray-30">
+        <form className="h-12 w-full max-w-[800px] sm:h-16 flex bg-gray-0 rounded-[13.71px] items-center px-3 sm:px-4 border-[2.67px] border-gray-30">
           <button className="bg-[url('/iconssite/search.svg')] bg-no-repeat bg-center bg-contain size-6 sm:size-8 bg-transparent" />
           <input
             placeholder="Поиск по базе знаний"
@@ -29,8 +29,8 @@ const Hero = () => {
 
 const Parts = () => {
   return (
-    <section className="p-4 sm:px-40 sm:py-16">
-      <ul className="grid w-full gap-x-8 gap-y-8 sm:gap-y-12 grid-cols-1 sm:grid-cols-[repeat(auto-fit,_minmax(512px,_1fr))]">
+    <section className="p-4 xs:p-8 lg:px-40 md:py-16">
+      <ul className="grid w-full gap-8 md:gap-y-12 grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))] sm:grid-cols-[repeat(auto-fill,_minmax(400px,_1fr))]">
         {partList.map((item, index) => (
           <li className="w-full flex" key={index}>
             <Image
@@ -38,7 +38,7 @@ const Parts = () => {
               alt={item.icon}
               width={0}
               height={0}
-              className="mb-auto size-12"
+              className="mb-auto size-12 sm:size-16"
             />
             <div className="ml-4 sm:ml-8">
               <h3 className="text-[24px] sm:text-[32px] leading-[31.2px] sm:leading-[41.6px] text-brand-80 font-medium">
@@ -66,12 +66,12 @@ const Parts = () => {
 const CTA = () => {
   return (
     <section className="flex flex-col justify-center items-center py-16 px-8 sm:py-32">
-      <h3 className="font-medium text-[24px] sm:text-[64px] leading-[31.2px] sm:leading-[83.2px] text-gray-100">
+      <h3 className="font-medium text-[24px] sm:text-[40px] lg:text-[64px] leading-[31.2px] sm:leading-[52px] lg:leading-[83.2px] text-gray-100">
         Не нашлось ответа?
       </h3>
-      <Button className="h-8 sm:h-[84px] font-medium text-sm sm:text-[30px] sm:leading-[42px] rounded-md sm:rounded-[18px] pl-3 sm:pl-9 pr-2 sm:pr-[18px] mt-4 sm:mt-8">
+      <Button className="h-8 sm:h-[56px] lg:h-[84px] font-medium text-sm sm:text-xl lg:text-[30px] lg:leading-[42px] rounded-md sm:rounded-xl lg:rounded-[18px] pl-3 sm:pl-6 lg:pl-9 pr-2 sm:pr-3 lg:pr-[18px] mt-4 sm:mt-8">
         Написать в телеграм
-        <span className="bg-[url('/iconssite/telegram-no-circle.svg')] bg-no-repeat bg-center bg-contain size-4 sm:size-12 inline-block ml-[6px] sm:ml-6" />
+        <span className="bg-[url('/iconssite/telegram-no-circle.svg')] bg-no-repeat bg-center bg-contain size-4 sm:size-8 lg:size-12 inline-block ml-[6px] sm:ml-4 lg:ml-6" />
       </Button>
     </section>
   );
