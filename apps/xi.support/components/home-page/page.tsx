e6da0fe60cb69c7199500595a0690f90284960c0
Vitaly Kharlakov <vkharlakov@Vitalys-MacBook-Air.local>
2024-02-25 13:29:01 +0100

    feat(29044294): rework search form in Hero; fix CTA bp

--- apps/xi.support/components/home-page/page.tsx
+++ apps/xi.support/components/home-page/page.tsx
@@ -15,11 +15,11 @@ const Hero = () => {
         <p className="text-[14px] leading-[18.2px] mb-4 sm:text-[20px] sm:leading-[32px] md:text-2xl font-normal sm:mb-8 text-gray-90 sm:text-gray-10">
           Воспользуйтесь поиском или перейдите в нужный раздел
         </p>
-        <form className="h-12 w-full max-w-[800px] sm:h-16 flex bg-gray-0 rounded-[13.71px] items-center px-3 sm:px-4 border-[2.67px] border-gray-30">
-          <button className="bg-[url('/iconssite/search.svg')] bg-no-repeat bg-center bg-contain size-6 sm:size-8 bg-transparent" />
+        <form className="h-12 relative w-full max-w-[800px] sm:h-16">
+          <button className="absolute z-10 top-[50%] translate-y-[-50%] left-3 sm:left-4 bg-[url('/iconssite/search.svg')] bg-no-repeat bg-center bg-contain size-6 sm:size-8 bg-transparent" />
           <input
             placeholder="Поиск по базе знаний"
-            className="w-full h-full border-none text-[16px] sm:text-[21.33px] font-normal leading-[22px] sm:leading-[29.33px] ml-2 sm:ml-[10.67px] focus:outline-none"
+            className="w-full rounded-[13.71px] border-2 sm:border-[2.67px] pl-[44px] sm:pl-[58.67px] p-3 sm:py-4 border-gray-30 h-full text-[16px] sm:text-[21.33px] font-normal leading-[22px] sm:leading-[29.33px]"
           />
         </form>
       </div>
@@ -66,12 +66,12 @@ const Parts = () => {
 const CTA = () => {
   return (
     <section className="flex flex-col justify-center items-center py-16 px-8 sm:py-32">
-      <h3 className="font-medium text-[24px] sm:text-[40px] lg:text-[64px] leading-[31.2px] sm:leading-[52px] lg:leading-[83.2px] text-gray-100">
+      <h3 className="font-medium text-[24px] sm:text-[40px] xl:text-[64px] leading-[31.2px] sm:leading-[52px] xl:leading-[83.2px] text-gray-100">
         Не нашлось ответа?
       </h3>
-      <Button className="h-8 sm:h-[56px] lg:h-[84px] font-medium text-sm sm:text-xl lg:text-[30px] lg:leading-[42px] rounded-md sm:rounded-xl lg:rounded-[18px] pl-3 sm:pl-6 lg:pl-9 pr-2 sm:pr-3 lg:pr-[18px] mt-4 sm:mt-8">
+      <Button className="h-8 sm:h-[56px] xl:h-[84px] font-medium text-sm sm:text-xl xl:text-[30px] xl:leading-[42px] rounded-md sm:rounded-xl xl:rounded-[18px] pl-3 sm:pl-6 xl:pl-9 pr-2 sm:pr-3 xl:pr-[18px] mt-4 sm:mt-8">
         Написать в телеграм
-        <span className="bg-[url('/iconssite/telegram-no-circle.svg')] bg-no-repeat bg-center bg-contain size-4 sm:size-8 lg:size-12 inline-block ml-[6px] sm:ml-4 lg:ml-6" />
+        <span className="bg-[url('/iconssite/telegram-no-circle.svg')] bg-no-repeat bg-center bg-contain size-4 sm:size-8 xl:size-12 inline-block ml-[6px] sm:ml-4 xl:ml-6" />
       </Button>
     </section>
   );
