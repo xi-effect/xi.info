0912925f348f94996179cf94cb21cbed2adb92cf
unknownproperty <igor.buksheff@yandex.ru>
2024-03-28 17:12:29 +0300

    feat: some fixes formobile xi.land

--- apps/xi.land/components/MobileMenu.tsx
+++ apps/xi.land/components/MobileMenu.tsx
@@ -12,61 +12,65 @@ interface IPropMobileHeader {
 }
 
 export const MobileMenu = ({ links, toggleBurgerMenu }: IPropMobileHeader) => (
-  <main className="sm:relative">
-    <ModalContent variant="full">
-      <ModalHeader className="border-none">
-        <ModalCloseButton
-          onClick={() => toggleBurgerMenu()}
-          className="h-10 w-10 ml-auto sm:absolute bg-transparent flex p-2 sm:top-0 xl:top-0 right-[16px] sm:right-0 xl:right-[-56px]"
-          variant="full"
-        >
-          <Close />
-        </ModalCloseButton>
-        <ModalTitle>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.2, duration: 1 }}
-          >
-            <Image alt="xieffect logo" src="/xieffectlight.webp" height={24} width={202} />
-          </motion.div>
-        </ModalTitle>
-      </ModalHeader>
-      <div className="px-7">
-        {links &&
-          links.map((item) => (
-            <motion.div
-              key={item.link}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 0.2, duration: 1 }}
-              className="w-full md:w-fit flex flex-col sm:flex-row gap-4 mb-8 "
-            >
-              <NextLink className="opacity-40 text-[24px]" href={item.link}>
-                {item.label}
-              </NextLink>
-            </motion.div>
-          ))}
-      </div>
-      <ModalFooter className="border-none flex flex-col align-bottom justify-center">
+  <ModalContent className="h-[100dvh]" variant="full">
+    <ModalHeader className="border-none">
+      <ModalCloseButton
+        onClick={() => toggleBurgerMenu()}
+        className="h-10 w-10 ml-auto sm:absolute bg-transparent flex p-2 sm:top-0 xl:top-0 right-[16px] sm:right-0 xl:right-[-56px]"
+        variant="full"
+      >
+        <Close />
+      </ModalCloseButton>
+      <ModalTitle>
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.2, duration: 1 }}
-          className="flex items-center w-full gap-10"
         >
-          <div className="w-2/6">
-            <Button className="w-full p-5" size="s" type="submit">
-              Войти
-            </Button>
-          </div>
-          <div className="w-4/6">
-            <Button size="s" className="w-full p-5" variant="secondary">
-              Зарегистрироваться
-            </Button>
-          </div>
+          <Image alt="xieffect logo" src="/xieffectlight.webp" height={24} width={202} />
         </motion.div>
-      </ModalFooter>
-    </ModalContent>
-  </main>
+      </ModalTitle>
+    </ModalHeader>
+    <div className="px-7">
+      {links &&
+        [
+          {
+            label: 'Главная',
+            link: '/',
+          },
+          ...links,
+        ].map((item) => (
+          <motion.div
+            key={item.link}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.2, duration: 1 }}
+            className="w-full md:w-fit flex flex-col sm:flex-row gap-4 mb-8 "
+          >
+            <NextLink className="opacity-40 text-[24px]" href={item.link}>
+              {item.label}
+            </NextLink>
+          </motion.div>
+        ))}
+    </div>
+    <ModalFooter className="border-none flex flex-col align-bottom justify-center">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.2, duration: 1 }}
+        className="flex items-center w-full gap-10"
+      >
+        <div className="w-2/6">
+          <Button className="w-full p-5" size="s" type="submit">
+            Войти
+          </Button>
+        </div>
+        <div className="w-4/6">
+          <Button size="s" className="w-full p-5" variant="secondary">
+            Зарегистрироваться
+          </Button>
+        </div>
+      </motion.div>
+    </ModalFooter>
+  </ModalContent>
 );
