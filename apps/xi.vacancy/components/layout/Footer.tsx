8685134fa1113de01d65b5c73228e542c9d5153f
Julia Bednaia <juliabednaia@gmail.com>
2024-07-10 17:11:49 +0300

    fix(33109747): added SendResumeModal to footer

--- apps/xi.vacancy/components/layout/Footer.tsx
+++ apps/xi.vacancy/components/layout/Footer.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import SendResumeButton from '../modal/SendResumeButton';
+import { Button } from '@xipkg/button';
+import SendResumeModal from '../modal/SendResumeModal';
 
 const Footer = () => (
   <footer className="mt-4 sm:mt-16">
@@ -26,7 +27,9 @@ const Footer = () => (
             Напиши пару слов о себе и о том, какая вакансия может быть вам интересна
           </p>
 
-          <SendResumeButton className="bg-brand-80 text-[15px] sm:text-[17px] text-gray-0 rounded-[6px] sm:rounded-[8px] py-[5px] px-4 sm:py-[12px] sm:px-[24px]" />
+          <SendResumeModal>
+            <Button className="w-[165px] sm:w-[200px]">Отправить резюме</Button>
+          </SendResumeModal>
         </div>
       </div>
       <div className="py-4 sm:py-8 xl:py-16 sm:px-8 xl:px-[48px] flex flex-col min-[1000px]:flex-row min-[1000px]:content-center  justify-between">
