2acec7a82627f0ff51024b2e98c4d863a1ac342e
unknownproperty <igor.buksheff@yandex.ru>
2024-08-25 20:01:33 +0300

    fix: return webpack config

--- apps/xi.vacancy/next.config.js
+++ apps/xi.vacancy/next.config.js
@@ -45,23 +45,23 @@ const nextConfig = {
     ],
   },
   output: 'export',
-  // webpack(config) {
-  //   config.module.rules.push({
-  //     test: /\.(woff|woff2|eot|ttf|otf)$/i,
-  //     issuer: { and: [/\.(js|ts|md)x?$/] },
-  //     type: 'asset/resource',
-  //   });
-  //   config.module.rules.push({
-  //     test: /\.d.ts?$/,
-  //     use: [
-  //       {
-  //         loader: 'ignore-loader',
-  //       },
-  //     ],
-  //   });
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      type: 'asset/resource',
+    });
+    config.module.rules.push({
+      test: /\.d.ts?$/,
+      use: [
+        {
+          loader: 'ignore-loader',
+        },
+      ],
+    });
 
-    // return config;
-  // },
+    return config;
+  },
 };
 
 module.exports = () => plugins.reduce((acc, next) => next(acc), nextConfig);
