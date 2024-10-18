e1b24446a60a67f4d7ca61eb66d630fc299c0421
d9m0n4 <ched08@yandex.ru>
2024-10-18 15:15:31 +0300

    fix(40126263): return product page/assets, fix prettier dependency

---
+++ apps/xi.land/app/product/page.tsx
@@ -0,0 +1,18 @@
+import { Items } from 'components/productPage/Items';
+import { Title } from 'components/productPage/Title';
+
+export const metadata = {
+  title: 'продукт | xi.effect',
+  description: 'Приложение для репетиторов и малого бизнеса',
+};
+
+export default function ProductPage() {
+  return (
+    <main className="flex flex-row h-full w-full min-h-[100vh] justify-center items-start bg-gray-0 overflow-x-hidden">
+      <div className="flex flex-col w-full justify-start items-start">
+        <Title />
+        <Items />
+      </div>
+    </main>
+  );
+}
