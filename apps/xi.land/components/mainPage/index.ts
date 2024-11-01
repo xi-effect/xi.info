1c40ef49c1913c679bc31079a3429203b579d996
d9m0n4 <ched08@yandex.ru>
2024-11-02 00:14:41 +0300

    feat(40149567): test and tasks block

--- apps/xi.land/components/mainPage/index.ts
+++ apps/xi.land/components/mainPage/index.ts
@@ -3,3 +3,4 @@ export { HeroContent } from './firstScreen/HeroContent';
 export { LightningShape } from './firstScreen/LightningShape';
 export { StarShape } from './firstScreen/StarShape';
 export { WaveShape } from './firstScreen/WaveShape';
+export { Tasks } from './tasks/Tasks';
---
+++ apps/xi.land/components/mainPage/tasks/Tasks.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import { Button } from '@xipkg/button';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+
+const descriptionBlocks = [
+  { image: '1.webp', label: '3 типа вопросов' },
+  { image: '2.webp', label: 'Автоматическая проверка' },
+  { image: '3.webp', label: 'Встроенный редактор' },
+  { image: '4.webp', label: 'Отправка файлов' },
+];
+
+export const Tasks = () => (
+  <div className="py-8 px-4 flex flex-col gap-8 xs:p-8 xl:py-16 xl:px-24">
+    <div className="flex flex-col gap-2 text-center">
+      <h5 className="text-h5 xs:text-h3 font-medium">Тесты и задания</h5>
+      <p className="text-l-base xs:text-h6 text-gray-80">Быстро оценивайте знания</p>
+    </div>
+    <div className="grid gap-8 lg:grid-cols-2 lg:grid-rows-2 2xl:gap-16">
+      <motion.div
+        initial={{ y: 60, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ delay: 0.3, duration: 0.2 }}
+        className="row-span-2 rounded-[32px] bg-violet-100 p-8 flex flex-col gap-2 max-xs:bg-none bg-cover bg-[url('/assets/mainPage/tasks/line.svg')] bg-no-repeat bg-right-top"
+      >
+        <div className="flex">
+          <Image
+            src="/assets/mainPage/tasks/main.webp"
+            alt="text and tasks main img"
+            width={768}
+            height={679}
+            className="hidden xs:block xl:h-[460px] xl:w-[520px] 2xl:h-auto 2xl:w-auto"
+          />
+          <Image
+            src="/assets/mainPage/tasks/mainMobile.webp"
+            alt="text and tasks main img"
+            width={279}
+            height={182}
+            className="xs:hidden block"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <h3 className="text-xl-base xs:text-h6 xl:text-h5 text-gray-0">Живое общение</h3>
+          <p className="text-s-base text-gray-0 xs:text-l-base xl:text-xl-base">
+            Обсуждайте задания и тесты
+          </p>
+        </div>
+      </motion.div>
+      <div className="row-span-2 grid min-[700px]:grid-cols-2 grid-rows-2 gap-4 xs:gap-8 2xl:gap-16">
+        {descriptionBlocks.map((item, index) => (
+          <motion.div
+            initial={{ y: 60, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ delay: Number(`0.${6 + index}`), duration: 0.3 }}
+            key={item.image}
+            className="flex flex-col sm:px-2 xs:gap-2 justify-between xs:py-8 xs:bg-violet-20 xs:rounded-[32px] xs:h-[302px] sm:w-auto sm:h-auto 2xl:p-8"
+          >
+            <div className="hidden flex-1 xs:flex justify-center items-center ">
+              <Image
+                src={`/assets/mainPage/tasks/${item.image}`}
+                width={100}
+                height={100}
+                priority
+                className="xs:block w-auto h-auto lg:max-h-[160px] xl:max-h-[184px] 2xl:max-h-none"
+                alt="test and tasks"
+              />
+            </div>
+            <p className="text-xl-base 2xl:text-h6 font-medium xs:text-center">{item.label}</p>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+    <motion.div
+      initial={{ y: 60, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      viewport={{ once: true }}
+      transition={{ delay: 0.3, duration: 0.2 }}
+      className="flex flex-col gap-2 xs:flex-wrap xs:flex-row justify-center xs:gap-8"
+    >
+      <Button
+        size="l"
+        className="bg-violet-100 h-12 xs:text-xl-base xs:h-[72px] xs:rounded-2xl xs:px-9 rounded-lg hover:bg-violet-60 active:bg-violet-60 focus:bg-violet-60 2xl:h-[84px] xl:rounded-3xl 2xl:text-[30px] 2xl:px-12"
+      >
+        Начать работать
+      </Button>
+      <Button
+        variant="secondary"
+        className="text-l-base xs:text-xl-base xs:h-[72px] xs:rounded-2xl xs:px-9 2xl:h-[84px] xl:rounded-3xl 2xl:text-[30px] 2xl:px-12"
+      >
+        Узнать больше
+      </Button>
+    </motion.div>
+  </div>
+);
