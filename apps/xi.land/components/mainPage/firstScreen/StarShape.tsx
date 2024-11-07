a3c32657ff5ff6efcd42db2aad7c784d9ea95706
unknownproperty <igor.buksheff@yandex.ru>
2024-11-08 00:42:00 +0300

    fix(40973477): mobile layout

--- apps/xi.land/components/mainPage/firstScreen/StarShape.tsx
+++ apps/xi.land/components/mainPage/firstScreen/StarShape.tsx
@@ -1,13 +1,11 @@
 export const StarShape = () => (
   <svg
-    // width="393"
-    // height="342"
     viewBox="40 -100 520 602"
     fill="none"
     strokeLinecap="round"
     strokeLinejoin="round"
     xmlns="http://www.w3.org/2000/svg"
-    className="scale-[1.3] md:scale-[1.7] stroke-[16] w-[415px] 2xl:w-[463px] 2xl:h-[560px] 2xl:-top-[65px] 2xl:left-[0] h-[240px] pointer-events-none opacity-10 absolute -top-[20px] -left-[40px] xs:top-0 xs:left-0"
+    className="scale-[1.3] md:scale-[1.7] stroke-[16] w-[290px] 2xl:w-[463px] 2xl:h-[560px] 2xl:-top-[65px] 2xl:left-[0] h-[240px] pointer-events-none opacity-10 absolute -top-[20px] -left-[40px] xs:top-0 xs:left-0"
   >
     <path
       d="M242.786 -11.3992L219.017 -135M285.372 -44.1609L300.723 -106.209M285.372 266.082L318.055 426.415M242.786 315.224L232.387 382.237M383.419 70.8594L478 21.3624M121.96 70.8594L15 3.49241M75.4128 95.8207H24.9037M145.729 219.421L54.1198 296.362M393.818 194.815L466.611 231.867M256.785 70.8594L333.365 3.49241L310.525 108.585L403.227 134.858L310.525 161.131L333.365 231.867L264.175 194.815L197 286.434L205.061 177.299L75.4128 154.395L185.344 108.585L164.084 8.88177L256.785 70.8594Z"
