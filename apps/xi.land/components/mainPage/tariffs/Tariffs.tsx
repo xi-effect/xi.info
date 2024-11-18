300ef429cf9fddb8495d62c0cd1f18c1d70cbc79
d9m0n4 <ched08@yandex.ru>
2024-11-19 00:30:39 +0300

    fix(41663916): fix tariffs blocks sizes

--- apps/xi.land/components/mainPage/tariffs/Tariffs.tsx
+++ apps/xi.land/components/mainPage/tariffs/Tariffs.tsx
@@ -16,7 +16,7 @@ const tariffs = [
     buttonLabel: 'Начать пользоваться',
     backgroundColor: 'bg-violet-20',
     titleColor: 'text-violet-100',
-    buttonClassName: 'bg-violet-100 hover:bg-violet-60',
+    buttonClassName: 'bg-violet-100 hover:bg-violet-60 active:bg-violet-60 focus:bg-violet-60',
   },
   {
     id: 2,
@@ -27,7 +27,7 @@ const tariffs = [
     buttonLabel: 'Попробовать бесплатно',
     backgroundColor: 'bg-green-0',
     titleColor: 'text-green-80',
-    buttonClassName: 'bg-green-80 hover:bg-green-60',
+    buttonClassName: 'bg-green-80 hover:bg-green-60 active:bg-green-60 focus:bg-green-60',
   },
   {
     id: 3,
@@ -38,37 +38,37 @@ const tariffs = [
     buttonLabel: 'Попробовать бесплатно',
     backgroundColor: 'bg-orange-0',
     titleColor: 'text-orange-80',
-    buttonClassName: 'bg-orange-80 hover:bg-orange-60',
+    buttonClassName: 'bg-orange-80 hover:bg-orange-60 active:bg-orange-60 focus:bg-orange-60',
   },
 ];
 
 export const Tariffs = () => (
-  <section className="py-8 xs:py-16 md:px-8 xl:px-24">
+  <section className="py-8 sm:py-16 md:px-8 xl:px-24">
     <div className="flex flex-col gap-8 overflow-hidden">
-      <div className="flex-col flex md:flex-row gap-8 px-8 md:px-0 lg:px-8 2xl:px-16 justify-between">
+      <div className="flex-col flex md:flex-row gap-8 px-8 md:px-0 lg:px-8 3xl:px-16 justify-between">
         <div className="flex flex-col gap-2 items-start">
-          <h2 className="text-h6 xs:text-h5 lg:text-h3 2xl:text-h2">
+          <h2 className="text-h6 sm:text-h5 lg:text-h3 3xl:text-h2">
             Месяц бесплатно на любом тарифе
           </h2>
           <Link
             href="#"
             variant="none"
             size="l"
-            className="group text-brand-80 xs:text-l-base md:text-m-base lg:text-l-base 2xl:text-[30px] flex items-center 2xl:gap-4 gap-2"
+            className="group text-brand-80 sm:text-l-base md:text-m-base lg:text-l-base 3xl:text-[30px] flex items-center 3xl:gap-4 gap-2"
           >
             Сравнить тарифы
-            <ArrowRight className="group-hover:fill-gray-100 fill-brand-80 xs:w-8 xs:h-8 md:w-6 md:h-6 lg:w-8 lg:h-8 2xl:w-12 2xl:h-12" />
+            <ArrowRight className="group-hover:fill-gray-100 fill-brand-80 sm:w-8 sm:h-8 md:w-6 md:h-6 lg:w-8 lg:h-8 3xl:w-12 3xl:h-12" />
           </Link>
         </div>
-        <ul className="flex xl:basis-1/3 flex-col gap-2 xl:pl-4">
-          <li className="flex gap-2 items-center text-gray-90 xs:text-l-base md:text-m-base 2xl:text-l-base">
-            <Check className="xs:w-5 xs:h-5 fill-gray-60" /> Надежная система для работы
+        <ul className="flex md:basis-1/3 flex-col gap-2 xl:pl-4">
+          <li className="flex gap-2 items-center text-gray-90 sm:text-l-base lg:text-m-base md:text-s-base 3xl:text-l-base">
+            <Check className="sm:w-5 sm:h-5 fill-gray-60" /> Надежная система для работы
           </li>
-          <li className="flex gap-2 items-center text-gray-90 xs:text-l-base md:text-m-base 2xl:text-l-base">
-            <Check className="xs:w-5 xs:h-5 fill-gray-60" /> Ежемесячные обновления
+          <li className="flex gap-2 items-center text-gray-90 sm:text-l-base lg:text-m-base md:text-s-base 3xl:text-l-base">
+            <Check className="sm:w-5 sm:h-5 fill-gray-60" /> Ежемесячные обновления
           </li>
-          <li className="flex gap-2 items-center text-gray-90 xs:text-l-base md:text-m-base 2xl:text-l-base">
-            <Check className="xs:w-5 xs:h-5 fill-gray-60" /> Круглосуточная поддержка
+          <li className="flex gap-2 items-center text-gray-90 sm:text-l-base lg:text-m-base md:text-s-base 3xl:text-l-base">
+            <Check className="sm:w-5 sm:h-5 fill-gray-60" /> Круглосуточная поддержка
           </li>
         </ul>
       </div>
@@ -90,7 +90,7 @@ export const Tariffs = () => (
           ))}
         </CarouselContent>
       </Carousel>
-      <div className="hidden md:grid grid-cols-3 gap-8">
+      <div className="hidden md:grid md:grid-cols-3 gap-8">
         {tariffs.map((tariff, index) => (
           <motion.div
             initial={{ y: 80, opacity: 0 }}
--- apps/xi.land/components/mainPage/tariffs/TariffsItem.tsx
+++ apps/xi.land/components/mainPage/tariffs/TariffsItem.tsx
@@ -23,34 +23,40 @@ export const TariffsItem = ({
   titleClassName,
 }: TariffsItemPropsT) => (
   <div
-    className={`p-6 xs:p-16 md:p-6 gap-y-8 xl:p-8 2xl:p-16 rounded-[32px] bg-brand-0 flex flex-col justify-between h-full ${containerClassName}`}
+    className={`p-6 sm:p-16 md:p-6 gap-y-8 sm:gap-0 xl:p-8 3xl:p-16 rounded-[32px] bg-brand-0 flex flex-col justify-between h-full ${containerClassName}`}
   >
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 md:gap-6 lg:gap-8">
       <div className="flex flex-col gap-2">
-        <h3 className={`text-h5 xs:text-h2 md:text-h3 lg:text-h2 ${titleClassName}`}>{title}</h3>
-        <p className="text-l-base xs:text-xl-base md:text-m-base lg:text-xl-base text-gray-90">
+        <h3
+          className={`text-h5 sm:text-h2 md:text-h6 lg:text-h4 font-medium 3xl:text-h2 ${titleClassName}`}
+        >
+          {title}
+        </h3>
+        <p className="text-l-base sm:text-xl-base md:text-s-base lg:text-[18px] xl:text-l-base 3xl:text-xl-base text-gray-90">
           {description}
         </p>
       </div>
-      <ul>
+      <ul className="flex flex-col gap-2">
         {features.map((feature, index) => (
           <li
             key={index}
-            className="flex items-center gap-x-2 xs:text-l-base md:text-m-base lg:text-l-base text-gray-90"
+            className="flex items-center gap-x-2 sm:text-l-base md:text-s-base lg:text-m-base 3xl:text-l-base text-gray-90"
           >
-            <Check className="xs:w-5 xs:h-5 md:w-4 md:h-4 lg:w-5 lg:h-5 fill-gray-60" />
+            <Check className="sm:w-5 sm:h-5 md:w-4 md:h-4 3xl:w-5 3xl:h-5 fill-gray-60" />
             {feature}
           </li>
         ))}
       </ul>
     </div>
-    <div className="flex flex-col pt-12 gap-6 xs:mt-0">
-      <h4 className="text-h5 xs:text-h1 md:text-h3 lg:text-h2 2xl:text-h1">
+    <div className="flex flex-col pt-12 md:pt-8 3xl:pt-12 gap-6 md:gap-4 lg:gap-6">
+      <h4 className="text-h5 sm:text-h1 font-medium md:text-h5 lg:text-h3 3xl:text-h1">
         {price.amount}
-        {price.unit && <span className="text-gray-80 text-base xs:text-h5">{price.unit}</span>}
+        {price.unit && (
+          <span className="text-gray-80 font-normal text-l-base sm:text-h5">{price.unit}</span>
+        )}
       </h4>
       <Button
-        className={`rounded-3xl xs:h-14 md:h-12 md:text-m-base lg:text-l-base lg:h-14 xs:rounded-[56px] xs:text-l-base ${buttonClassName}`}
+        className={`rounded-3xl sm:h-14 md:h-10 lg:h-12 md:text-s-base lg:text-m-base 3xl:text-l-base 3xl:h-14 sm:rounded-[56px] sm:text-l-base ${buttonClassName}`}
       >
         {buttonLabel}
       </Button>
