c93b9f1a303b01c9f5e0d86bd8572dfa97c44732
unknownproperty <igor.buksheff@yandex.ru>
2025-03-21 14:43:09 +0300

    chore: add pm junior and middle vacancy

--- apps/xi.vacancy/components/common/const.ts
+++ apps/xi.vacancy/components/common/const.ts
@@ -121,19 +121,19 @@ export const vacancyList: ProfessionOptionsT[] = [
     id: 'design-graphic',
     title: 'Графический дизайнер',
   },
-  // {
-  //   label: 'Product manager',
-  //   author: {
-  //     name: 'Игорь',
-  //     tg: '@unknownproperty',
-  //     grade: 'CEO, Сo-Founder',
-  //     src: '/vacancy/unknownproperty.jpg',
-  //   },
-  //   date: '2 февраля',
-  //   grade: 'Junior',
-  //   id: 'product-pm',
-  //   title: 'Product manager',
-  // },
+  {
+    label: 'Product manager',
+    author: {
+      name: 'Игорь',
+      tg: '@unknownproperty',
+      grade: 'CEO, Сo-Founder',
+      src: '/vacancy/unknownproperty.jpg',
+    },
+    date: '2 февраля',
+    grade: 'Junior, Middle',
+    id: 'product-pm',
+    title: 'Product manager',
+  },
   // {
   //   label: 'Business analyst',
   //   author: {
