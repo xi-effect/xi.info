a601e6af06b7b05b6d1411432a4182aaacd96a74
xyzelena <emkhuzhina@gmail.com>
2025-09-22 14:01:31 +0300

    feat(18): upgrade Sticker component

--- apps/xi.land/components/main/Benefits/Benefits.tsx
+++ apps/xi.land/components/main/Benefits/Benefits.tsx
@@ -2,16 +2,41 @@ import { Sticker } from './Sticker';
 
 export const Benefits = () => {
   return (
-    <section className="bg-emerald-300 relative w-full m-h-[915px] overflow-hidden px-4 py-8 xs:px-6 sm:px-12 sm:py-20 lg:px-16 flex items-center justify-center">
-      <div className="bg-gray-100 h-full w-full grid grid-cols-1 grid-rows-3 gap-[10px] sm:grid-cols-3 sm:grid-rows-2 sm:gap-6 items-stretch sm:place-content-center">
-        <Sticker className="bg-pink-500 row-start-1">pink</Sticker>
-
-        <Sticker className="bg-green-500 col-start-2 row-start-2 xs:row-start-3 xs:col-start-1 sm:row-start-2 sm:col-start-2 sm:-translate-y-[20%] lg:-translate-y-[50%]">
-          green
+    <section
+      className="relative w-full min-h-[915px] overflow-hidden flex items-center justify-center px-4 py-8 xs:px-6 sm:px-12 sm:py-20 lg:px-16 bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: "url('/assets/main/benefits/bg_benefits.svg')" }}
+    >
+      <div className="h-full w-full grid grid-cols-1 grid-rows-3 gap-[10px] sm:grid-cols-3 sm:grid-rows-2 sm:gap-6 items-stretch sm:place-content-center">
+        <Sticker className="row-start-1" bgColor="violet">
+          <div className="flex flex-col items-center justify-center text-center gap-2">
+            <div>Быстрый переход между инструментами</div>
+            <div>+</div>
+            <div>Единая рабочая среда</div>
+            <div>=</div>
+            <div>Экономия до 2 часов в неделю</div>
+          </div>
         </Sticker>
 
-        <Sticker className="bg-yellow-500 row-start-3 xs:row-start-2 xs:col-start-2 sm:row-start-1 sm:col-start-3">
-          yellow
+        <Sticker
+          className="col-start-2 row-start-2 xs:row-start-3 xs:col-start-1 sm:row-start-2 sm:col-start-2 sm:-translate-y-[20%] lg:-translate-y-[50%]"
+          bgColor="green"
+        >
+          <div className="flex flex-col items-start justify-center sm:justify-start gap-8">
+            <div>Ход опыта:</div>
+            <div>Шаг 1. Настройте платформу</div>
+            <div>Шаг 2. Удалите из преподавания скуку и рутину</div>
+            <div>Шаг 3. Оставьте главное — удовольствие от того, что ученик понял тему</div>
+          </div>
+        </Sticker>
+
+        <Sticker
+          className="row-start-3 xs:row-start-2 xs:col-start-2 sm:row-start-1 sm:col-start-3"
+          bgColor="yellow"
+        >
+          <div className="flex flex-col items-start justify-center sm:justify-start gap-6">
+            <div>Соберите материалы и учеников в одной платформе — и обретите дзен</div>
+            <div>Ваши знания бесценны. Делитесь ими с комфортом</div>
+          </div>
         </Sticker>
       </div>
     </section>
--- apps/xi.land/components/main/Benefits/Sticker.tsx
+++ apps/xi.land/components/main/Benefits/Sticker.tsx
@@ -2,16 +2,25 @@ import { cn } from '@xipkg/utils';
 
 export const Sticker = ({
   className,
+  bgColor,
   children,
 }: {
   className: string;
+  bgColor?: 'violet' | 'green' | 'yellow';
   children: React.ReactNode;
 }) => {
+  const violetBgColor = 'bg-violet-20 shadow-[4px_4px_10px_0px_rgba(131,48,196,0.2)]';
+  const greenBgColor = 'bg-green-0 shadow-[4px_4px_10px_0px_rgba(46,132,46,0.2)]';
+  const yellowBgColor = 'bg-yellow-20 shadow-[4px_4px_10px_0px_rgba(243,201,76,0.2)]';
+
   return (
     <div
       className={cn(
         'min-h-[218px] min-w-[200px]',
         'p-2 xs:p-4 sm:p-12 lg:px-8 lg:py-12',
+        bgColor === 'violet' && violetBgColor,
+        bgColor === 'green' && greenBgColor,
+        bgColor === 'yellow' && yellowBgColor,
         className,
       )}
     >
