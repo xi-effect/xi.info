25f50644f5dbd37d6ba1815c6fc00ca401ea2a71
xyzelena <emkhuzhina@gmail.com>
2025-09-23 00:29:32 +0300

    feat(18): update styles

--- apps/xi.land/components/main/Benefits/Benefits.tsx
+++ apps/xi.land/components/main/Benefits/Benefits.tsx
@@ -8,56 +8,58 @@ export const Benefits = () => {
 
   return (
     <section
-      className="relative w-full min-h-[915px] overflow-hidden flex items-center justify-center px-4 py-8 xs:px-6 sm:px-12 sm:py-20 lg:px-16 bg-no-repeat bg-cover bg-center"
+      className="relative w-full min-h-screen lg:min-h-[915px] overflow-hidden flex items-center justify-center bg-no-repeat bg-cover bg-center"
       style={{ backgroundImage: "url('/assets/main/benefits/bg_benefits.svg')" }}
     >
-      <div className="h-full w-full grid grid-cols-1 grid-rows-3 gap-[10px] sm:grid-cols-3 sm:grid-rows-2 sm:gap-6 items-stretch sm:place-content-center">
-        <Sticker className="row-start-1" bgColor="violet">
-          <div
-            className={cn(
-              fontSticker,
-              'flex flex-col items-center justify-center text-center xs:gap-2',
-            )}
-          >
-            <div>Быстрый переход между инструментами</div>
-            <div>+</div>
-            <div>Единая рабочая среда</div>
-            <div>=</div>
-            <div>Экономия до 2 часов в неделю</div>
-          </div>
-        </Sticker>
+      <div className="relative z-10 w-full mx-auto">
+        <div className="h-full w-full grid grid-cols-1 auto-rows-min gap-[10px] sm:grid-cols-3 sm:grid-rows-2 sm:gap-6 items-stretch sm:place-content-center">
+          <Sticker className="row-start-1" type="violetSticker">
+            <div
+              className={cn(
+                fontSticker,
+                'h-full flex flex-col items-center justify-center text-center xs:gap-2',
+              )}
+            >
+              <div>Быстрый переход между инструментами</div>
+              <div>+</div>
+              <div>Единая рабочая среда</div>
+              <div>=</div>
+              <div>Экономия до 2 часов в неделю</div>
+            </div>
+          </Sticker>
 
-        <Sticker
-          className="col-start-2 row-start-2 xs:row-start-3 xs:col-start-1 sm:row-start-2 sm:col-start-2 sm:-translate-y-[20%] lg:-translate-y-[50%]"
-          bgColor="green"
-        >
-          <div
-            className={cn(
-              fontSticker,
-              'flex flex-col items-start justify-center sm:justify-start gap-2 xs:gap-3 sm:gap-6',
-            )}
+          <Sticker
+            className="col-start-2 row-start-2 xs:row-start-3 xs:col-start-1 sm:row-start-2 sm:col-start-2 sm:-translate-y-[20%] lg:-translate-y-[50%]"
+            type="greenSticker"
           >
-            <div>Ход опыта:</div>
-            <div>Шаг 1. Настройте платформу</div>
-            <div>Шаг 2. Удалите из преподавания скуку и рутину</div>
-            <div>Шаг 3. Оставьте главное — удовольствие от того, что ученик понял тему</div>
-          </div>
-        </Sticker>
+            <div
+              className={cn(
+                fontSticker,
+                'h-full flex flex-col items-start justify-center sm:justify-start gap-2 xs:gap-3 sm:gap-6 lg:gap-8',
+              )}
+            >
+              <div>Ход опыта:</div>
+              <div>Шаг 1. Настройте платформу</div>
+              <div>Шаг 2. Удалите из преподавания скуку и рутину</div>
+              <div>Шаг 3. Оставьте главное — удовольствие от того, что ученик понял тему</div>
+            </div>
+          </Sticker>
 
-        <Sticker
-          className="row-start-3 xs:row-start-2 xs:col-start-2 sm:row-start-1 sm:col-start-3"
-          bgColor="yellow"
-        >
-          <div
-            className={cn(
-              fontSticker,
-              'flex flex-col items-start justify-center sm:justify-start gap-4 sm:gap-6',
-            )}
+          <Sticker
+            className="row-start-3 xs:row-start-2 xs:col-start-2 sm:row-start-1 sm:col-start-3"
+            type="yellowSticker"
           >
-            <div>Соберите материалы и учеников в одной платформе — и обретите дзен</div>
-            <div>Ваши знания бесценны. Делитесь ими с комфортом</div>
-          </div>
-        </Sticker>
+            <div
+              className={cn(
+                fontSticker,
+                'h-full flex flex-col items-start justify-center sm:justify-start gap-4 sm:gap-6',
+              )}
+            >
+              <div>Соберите материалы и учеников в одной платформе — и обретите дзен</div>
+              <div>Ваши знания бесценны. Делитесь ими с комфортом</div>
+            </div>
+          </Sticker>
+        </div>
       </div>
     </section>
   );
--- apps/xi.land/components/main/Benefits/Sticker.tsx
+++ apps/xi.land/components/main/Benefits/Sticker.tsx
@@ -1,30 +1,41 @@
 import { cn } from '@xipkg/utils';
 
+const stickerStyles = {
+  violetSticker: 'bg-violet-20 shadow-[4px_4px_10px_rgba(131,48,196,0.2)]',
+  greenSticker: 'bg-green-0 shadow-[4px_4px_10px_rgba(46,132,46,0.2)]',
+  yellowSticker: 'bg-yellow-20 shadow-[4px_4px_10px_rgba(243,201,76,0.2)]',
+} as const;
+
+const stickerSizes = {
+  violetSticker:
+    'xs:w-[320px] xs:h-[278px] sm:w-[378px] sm:h-[366px] lg:w-[453px] lg:h-[491px] xl:w-[490px] xl:h-[491px]',
+  greenSticker:
+    'xs:w-[313px] xs:h-[292px] sm:w-[378px] sm:h-[406px] lg:w-[453px] lg:h-[528px] xl:w-[490px] xl:h-[507px]',
+  yellowSticker:
+    'xs:w-[320px] xs:h-[278px] sm:w-[378px] sm:h-[366px] lg:w-[453px] lg:h-[480px] xl:w-[490px] xl:h-[480px]',
+} as const;
+
+type StickerType = keyof typeof stickerStyles;
+
 export const Sticker = ({
   className,
-  bgColor,
+  type,
   children,
 }: {
-  className: string;
-  bgColor?: 'violet' | 'green' | 'yellow';
+  className?: string;
+  type?: StickerType;
   children: React.ReactNode;
 }) => {
-  const violetBgColor = 'bg-violet-20 shadow-[4px_4px_10px_0px_rgba(131,48,196,0.2)]';
-  const greenBgColor = 'bg-green-0 shadow-[4px_4px_10px_0px_rgba(46,132,46,0.2)]';
-  const yellowBgColor = 'bg-yellow-20 shadow-[4px_4px_10px_0px_rgba(243,201,76,0.2)]';
-
   return (
     <div
       className={cn(
-        'min-h-[218px] min-w-[200px]',
-        'p-2 xs:p-4 sm:p-12 lg:px-8 lg:py-12',
-        bgColor === 'violet' && violetBgColor,
-        bgColor === 'green' && greenBgColor,
-        bgColor === 'yellow' && yellowBgColor,
+        'w-[258px] h-[218px] overflow-hidden',
+        type && stickerSizes[type],
+        type && stickerStyles[type],
         className,
       )}
     >
-      {children}
+      <div className="w-full h-full box-border p-2 xs:p-4 sm:p-8 lg:p-12">{children}</div>
     </div>
   );
 };
