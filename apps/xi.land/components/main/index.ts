09b699230056e4e9cc0a98707fb94ee8e3f0c819
unknownproperty <igor.buksheff@yandex.ru>
2026-05-17 01:50:25 +0300

    fix: try to build

--- apps/xi.land/components/main/index.ts
+++ apps/xi.land/components/main/index.ts
@@ -2,6 +2,6 @@ export { Hero } from './Hero';
 export { CapabilitiesBlock } from './Capabilities';
 export { TutorIdeasBlock } from './TutorIdeas';
 export { DevicesBlock } from './Devices';
-export { MessagesBlock } from './Messages/MessagesBlock';
+export { MessagesBlock } from './messages/MessagesBlock';
 export { CommunityBlock } from './Community/CommunityBlock';
 export { Faq } from './Faq';
